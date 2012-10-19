93653f9ed4e0be0a380100085c165e6d7c5207ba
egmkang wang <egmkang@gmail.com>
2012-10-19 23:42:03 +0800

    std::vector

--- python/libcxx/v1/printers.py
+++ python/libcxx/v1/printers.py
@@ -4,7 +4,7 @@ import re
 
 class CxxStringPrinter:
     "Print a std::__1::basic_string"
-    def __init__(self, val):
+    def __init__(self, typename, val):
         self.val = val
 
     def to_string(self):
@@ -19,6 +19,44 @@ class CxxStringPrinter:
 
     def display_hint (self):
         return "std::string"
+
+class CxxVectorPrinter:
+    "std::__1::vector"
+
+    class _iterator:
+        def __init__(self, begin, end):
+            self.begin = begin
+            self.end = end
+            self.count = 0
+
+        def __iter__(self):
+            return self
+        
+        def next(self):
+            count = self.count
+            self.count = self.count + 1
+            if self.begin == self.end:
+                raise StopIteration
+            value = self.begin.dereference()
+            self.begin = self.begin + 1
+            return ('[%d]' % count, value)
+
+    def __init__(self, typename, val):
+        self.val = val
+        self.typename = typename
+
+    def children(self):
+        return self._iterator(self.val['__begin_'],
+                              self.val['__end_'])
+
+    def to_string(self):
+        begin = self.val['__begin_']
+        end = self.val['__end_'] 
+        size = end - begin
+        return ('%s of length %d' % (self.typename, int(end - begin)))
+
+    def display_hint(self):
+        return 'std::vector'
 
 _type_parse_map = []
 
@@ -35,7 +73,7 @@ def lookup_type (val):
     for (regex, Printer) in _type_parse_map:
         m = regex.match(typename)
         if m is not None:
-            return Printer(val)
+            return Printer(typename, val)
     return None
 
 def register_libcxx_printers(obj):
@@ -43,6 +81,7 @@ def register_libcxx_printers(obj):
     if len(_type_parse_map) < 1:
         reg_function('^std::__1::basic_string<char,.*>$', CxxStringPrinter)
         reg_function('^std::__1::string.*$', CxxStringPrinter)
+        reg_function('^std::__1::vector<.*>$', CxxVectorPrinter)
 
     gdb.pretty_printers.append(lookup_type)
 
